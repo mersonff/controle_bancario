15c7776625158928dad5e006919ee7c3974950c4
Emerson Fernandes de Freitas <mersonsi@gmail.com>
2017-12-27 23:37:21 -0300

    Criando método para logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "Você deslogou da aplicação."
+		redirect_to root_path
+	end
 end
